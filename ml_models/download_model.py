8698d74cb03e67e2783bbc1f3d2100e135c5b6a5
fionn mcgoldrick <fionnmc123@gmail.com>
2026-03-06 10:02:21 +0000

    Removed old GraphCodeBERT files no longer needed.

--- ml_models/download_model.py
+++
@@ -1,8 +0,0 @@
-from transformers import AutoModel, AutoTokenizer
-
-MODEL_NAME = "microsoft/graphcodebert-base"
-
-print("Downloading GraphCodeBERT...")
-tokenizer = AutoTokenizer.from_pretrained(MODEL_NAME)
-model = AutoModel.from_pretrained(MODEL_NAME)
-print("Done. Model cached locally.")
